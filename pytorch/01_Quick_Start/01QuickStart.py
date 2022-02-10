8b4c6919eaad81e054b542daa1ff4a6d27a5ae31
Samuel <samuelbaco@gmail.com>
2022-02-10 11:07:32 -0300

    updated renv with torch

--- pytorch/01_Quick_Start/01QuickStart.py
+++ pytorch/01_Quick_Start/01QuickStart.py
@@ -1,3 +1,7 @@
 # LOAD LIBRARIES
 
-import 
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
